6b23b260e7755ffdba879d83ab359f8fe20ca5c6
Angie <amaticorena103@hotmail.com>
2021-12-22 20:55:19 -0500

    commit tree 44770325cb20f56803d69e63ad569c8f889939fd

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 /* Your Code Here */
 
+function createEmployeeRecord(array) {
+    const employeeObj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents:[],
+        timeOutEvents: []
+    }
+    return employeeObj
+}
+
+const createEmployeeRecords = function() {
+const newArr = []
+this.forEach(employee => {
+    let newEmployee= createEmployeeRecord(employee)
+    newArr.push(newEmployee)
+})
+return newArr
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
